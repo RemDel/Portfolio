547528aca835b4526b987c5b1546776c380d8c2c
RemDel <rem.del@live.fr>
2026-05-27 14:14:10 +0200

    Working NavLink

--- portfolio/src/components/Layout/index.js
+++ portfolio/src/components/Layout/index.js
@@ -1,15 +1,56 @@
-import Sidebar from '../Sidebar'
 import './index.scss';
 
-const Layout = () => {
-    return <><Sidebar/></> 
-    /*(
-     <div className='App'>/
-        <Sidebar />
-     </div>   
+import { NavLink } from "react-router-dom";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome } from '@fortawesome/free-solid-svg-icons'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faItchIo } from '@fortawesome/free-brands-svg-icons'
+
+
+function Sidebar() {
+    return (
+        <div className="nav-bar">
+        <nav>
+            {}
+            <NavLink exact="true" activeclassname="active" to="/">
+                <FontAwesomeIcon icon={faHome} color="#C5A187"/>
+            </NavLink>
+
+            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+                <FontAwesomeIcon icon={faUser} color="#C5A187"/>
+            </NavLink>
+
+            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+                <FontAwesomeIcon icon={faEnvelope} color="#C5A187"/>
+            </NavLink>
+        </nav>
+
+        <ul>
+            <li>
+                <a target="_blank" rel='noreferrer' href='https://www.linkedin.com/in/remi-delauzun'>
+                <FontAwesomeIcon icon={faLinkedin} color="#C5A187"/>
+                </a>
+            </li>
+            <li>
+                <a target="_blank" rel='noreferrer' href='https://demi-lune-azur.itch.io/'>
+                <FontAwesomeIcon icon={faItchIo} color="#C5A187"/>
+                </a>
+            </li>
+        </ul>
         
-        
-    )*/
+        </div>
+    );
+}
+
+function Layout() {
+    return (
+        <div>
+            <Sidebar />
+        </div>
+    );
 }
 
 export default Layout
--- portfolio/src/components/Sidebar/index.js
+++ portfolio/src/components/Sidebar/index.js
@@ -1,6 +1,6 @@
 //import { Link } from 'react-router-dom'
 //import { Container } from 'react-bootstrap';
-import './index.scss'
+/*import './index.scss'
 //import LogoDev from '../../assets/images/gamedevicon5.png'
 import { NavLink } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
@@ -11,36 +11,28 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import { faItchIo } from '@fortawesome/free-brands-svg-icons'
 
-const Sidebar = () => (
-    <div className="nav-bar">
+const Sidebar = () => {
+  return (
+      <div className="nav-bar">
       <nav>
-        <Link to="/">
+        <NavLink exact="true" activeclassname="active" to="/">
           <FontAwesomeIcon icon={faHome} color="#C5A187"/>
-        </Link>
-        <Link to="/about" className="about-link">
+        </NavLink>
+
+        <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
           <FontAwesomeIcon icon={faUser} color="#C5A187"/>
-        </Link>
-        <Link to="/contact" className="contact-link">
+        </NavLink>
+
+        <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
           <FontAwesomeIcon icon={faEnvelope} color="#C5A187"/>
-        </Link>
+        </NavLink>
       </nav>
-
-      <ul>
-        <li>
-          <a target="_blank" rel='noreferrer' href='https://www.linkedin.com/in/remi-delauzun'>
-            <FontAwesomeIcon icon={faLinkedin} color="#C5A187"/>
-          </a>
-        </li>
-        <li>
-          <a target="_blank" rel='noreferrer' href='https://demi-lune-azur.itch.io/'>
-            <FontAwesomeIcon icon={faItchIo} color="#C5A187"/>
-          </a>
-        </li>
-      </ul>
+      
     </div>
-)
+  )
+}
 
-export default Sidebar
+export default Sidebar*/
 
 /*
     <nav>
@@ -65,6 +57,79 @@ export default Sidebar
         <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
           <FontAwesomeIcon icon={faEnvelope} color="#C5A187"/>
         </NavLink>
+
+
+*/
+
+
+/*
+
+
+      <ul>
+        <li>
+          <a target="_blank" rel='noreferrer' href='https://www.linkedin.com/in/remi-delauzun'>
+            <FontAwesomeIcon icon={faLinkedin} color="#C5A187"/>
+          </a>
+        </li>
+        <li>
+          <a target="_blank" rel='noreferrer' href='https://demi-lune-azur.itch.io/'>
+            <FontAwesomeIcon icon={faItchIo} color="#C5A187"/>
+          </a>
+        </li>
+      </ul>
+
+
+*/
+
+/*
+
+            <nav>
+            {}
+            <NavLink
+                to="/"
+                style={({ isActive }) => ({
+                    color: isActive ? "red" : "black",
+                    fontWeight: isActive ? "bold" : "normal",
+                    textDecoration: "none",
+                    marginRight: "15px",
+                })}
+            >
+                <FontAwesomeIcon icon={faHome} color="#C5A187"/>
+            </NavLink>
+
+            <NavLink
+                to="/about"
+                style={({ isActive }) => ({
+                    color: isActive ? "red" : "black",
+                    fontWeight: isActive ? "bold" : "normal",
+                    textDecoration: "none",
+                })}
+            >
+                About
+            </NavLink>
+
+*/
+
+
+
+
+/*
+<div className="nav-bar">
+      <nav>
+        <NavLink exact="true" activeclassname="active" to="/">
+          <FontAwesomeIcon icon={faHome} color="#C5A187"/>
+        </NavLink>
+
+        <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+          <FontAwesomeIcon icon={faUser} color="#C5A187"/>
+        </NavLink>
+
+        <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+          <FontAwesomeIcon icon={faEnvelope} color="#C5A187"/>
+        </NavLink>
+      </nav>
+      
+    </div>
 
 
 */
